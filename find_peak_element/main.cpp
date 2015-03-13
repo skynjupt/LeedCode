71fff62065b208a4dd783d171b28fe778410d833
skynjupt <skynjupt@gmail.com>
2015-03-13 17:58:07 +0800

    add commit and title

--- find_peak_element/main.cpp
+++ find_peak_element/main.cpp
@@ -1,61 +1,30 @@
 /*
+	https://leetcode.com/problems/find-peak-element/
+	{
+		A peak element is an element that is greater than its neighbors.
 
+		Given an input array where num[i] ¡Ù num[i+1], find a peak element and return its index.
+
+		The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+		You may imagine that num[-1] = num[n] = -¡Þ.
+
+		For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+
+		Note:
+		Your solution should be in logarithmic complexity.
+	}
 */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-#if 0
-int findPeakElement(const vector<int> &num) {
-	int size = num.size();
-	if(size == 0)
-		return -1;
-	if(size == 1) {
-		return 0;
-	}
-	if(size >= 2 && num[0] > num[1]) {
-		return 0;
-	}
-	
-	int i = 1;
-	for(i = 1; i < size-1; i++) {
-		if(num[i-1] < num[i] && num[i] > num[i+1]) {
-			return i;
-		}
-	}
-	if(i == size-1 && num[i] > num[i-1]) {
-		return i;	
-	}
-	
-	return -1;
-}
-#endif
-
-#if 0
-int solve(const vector<int> &num, int low, int high) {
-	if(low == high) {
-		return low;
-	}	
-
-	int mid = (low+high)/2;
-	if(num[mid]<num[mid+1]) {
-		solve(num, mid+1, high);
-	} else {
-		solve(num, low, mid);
-	}
-
-}
-
-int findPeakElement(const vector<int> &num) {
-	return solve(num, 0, num.size()-1);	
-}
-#endif
-
-#if 0
+#if 1
 int findPeakElement(const vector<int> &num) {
 	int low = 0;
-	int high = num.size()-1;	
+	int high = num.size()-1;
+	// binary search, for each iteration, make sure num[low-1]< num[low] & num[high] > num[high+1] is true  
 	while(low < high) {	
 		int mid = (low+high)/2;
 		if(num[mid] < num[mid+1])
@@ -68,6 +37,7 @@ int findPeakElement(const vector<int> &num) {
 }
 #endif
 
+#if 0
 int findPeakElement(const vector<int> &num) {
 	for(int i = 1; i < num.size(); i++) {
 		if(num[i] < num[i-1]) {
@@ -76,6 +46,7 @@ int findPeakElement(const vector<int> &num) {
 	}	
 	return num.size()-1;
 }
+#endif
 
 int main(int argc, char *argv[])
 {
--- insertion-sort-list/main.cpp
+++ insertion-sort-list/main.cpp
@@ -1,3 +1,9 @@
+/*
+	https://leetcode.com/problems/insertion-sort-list/
+	{
+		Sort a linked list using insertion sort.
+	}
+*/
 #include <iostream>
 #include <vector>
 
